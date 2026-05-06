20ed89808785c9bb5803320dff60979e2c643f88
Sara Leal Silverio <saralealsilverio@gmail.com>
2026-05-06 12:54:11 -0600

    switching views and messages done

--- src/app/messages/message-edit/message-edit.ts
+++ src/app/messages/message-edit/message-edit.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, output, ViewChild, ElementRef } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -6,4 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './message-edit.html',
   styleUrl: './message-edit.css',
 })
-export class MessageEdit {}
+export class MessageEdit {
+  @ViewChild('subject') subjectInput!: ElementRef;
+  @ViewChild('msgText') msgTextInput!: ElementRef;
+  addMessageEvent = output<Message>();
+
+  onSendMessage(subject: string, msgText: string) {
+    let id = Math.random().toString();
+    let currentSender = 'Max';
+
+    let newMessage = new Message (id, subject, msgText, currentSender);
+    this.addMessageEvent.emit(newMessage);
+  }
+
+  onClear() {
+    this.subjectInput.nativeElement.value = '';
+    this.msgTextInput.nativeElement.value = '';
+  }
+}
--- src/app/messages/message-list/message-list.ts
+++ src/app/messages/message-list/message-list.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { Message } from '../message.model';
 import { MessageItem } from '../message-item/message-item';
 import { MessageEdit } from '../message-edit/message-edit';
-
 
 @Component({
   selector: 'cms-message-list',
@@ -9,4 +9,17 @@ import { MessageEdit } from '../message-edit/message-edit';
   templateUrl: './message-list.html',
   styleUrl: './message-list.css',
 })
-export class MessageList {}
+export class MessageList {
+  selectedMessageEvent = output<Message>();
+
+  messages: Message[] = [
+    new Message('1', 'Grading', 'this is a message about grading', 'Bro. Jackson'),
+    new Message('2', 'Assignment Due', 'Could we talk about assignment 3 due dates?', 'Steve Johnson'),
+    new Message('3', 'Class Cancelled', 'Class is being cancelled on Monday', 'Bro. Jackson')
+  ];
+
+  onAddMessage(message: Message) {
+    console.log(message);
+    this.messages.push(message);
+  }
+}
